cc072ef2eb843bf4abe9482024da95d4bfecff92
NamNV <imnamnv@gmail.com>
2023-07-02 17:15:27 +0700

    login logout feature

--- client/index.js
+++ client/index.js
@@ -4,16 +4,20 @@ import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { Router, hashHistory, Route, IndexRoute } from "react-router";
 import App from "./components/App";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import Dashboard from "./components/Dashboard";
+import requireAuth from "./components/requireAuth";
 
-// const networkInterface = createNetworkInterface({
-//   uri: "/graphql",
-//   opts: {
-//     credentials: "same-origin", //send the cookie
-//   },
-// });
+const networkInterface = createNetworkInterface({
+  uri: "/graphql",
+  opts: {
+    credentials: "same-origin", //send the cookie
+  },
+});
 
 const client = new ApolloClient({
-  // networkInterface,
+  networkInterface,
   dataIdFromObject: (o) => o.id,
 });
 
@@ -21,7 +25,11 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}></Route>
+        <Route path="/" component={App}>
+          <Router path="login" component={LoginForm} />
+          <Router path="signup" component={SignupForm} />
+          <Router path="dashboard" component={requireAuth(Dashboard)} />
+        </Route>
       </Router>
     </ApolloProvider>
   );
